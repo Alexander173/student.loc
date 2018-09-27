c182da60f916720b8c23dd05d4ea892a0f9d19b6
Alex <ghostfirelake@gmail.com>
2018-09-27 14:01:32 +0400

    27.09

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,21 +21,29 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
-$factory->defineAS(App\Student::class,'student',function (Faker $faker){
-    $faker=Faker\Factory::create('ru_RU');
+$factory->defineAS(App\Student::class,'assessment',function (Faker $faker){    
     return[
             'first_name'=>$faker->firstName,
             'middle_name'=>$faker->lastName,
-            'last_name'=>$faker->middleName,
+            'last_name'=>$faker->firstName,
             'date_of_birthday'=> $faker->dateTimeThisCentury->format('d-m-Y'),
-            'group_id'=>$faker->numberBetween($min=1,$max=3),
+            'group_id'=>$faker->randomElement(App\Group::pluck('id')->toArray()),
     ];
 });
-$factory->defineAS(App\Group::class,'student',function (Faker $faker){
-    $faker=Faker\Factory::create('ru_RU');
+$factory->defineAS(App\Group::class,'assessment',function (Faker $faker){
     return[
-            'group_name'=>$faker->,
-            'description'=>$faker->lastName,
-            
+            'group_name'=>$faker->unique()->randomElement($array=array('ITB','JSS','PHPUnit','PSS','JS','IBM','Apple','Samsung')),
+            'description'=>$faker->text($maxNbChrars=50),            
     ];
 });
+$factory->defineAs(App\Subject::class,'assessment',function(Faker $faker){
+    return[
+            'subject_name'=>$faker->unique()->randomElement($array=array('История', 'Русский язык','Математика')),
+    ];
+});
+$factory->defineAs(App\Assessment::class,'assessment',function(Faker $faker){
+    return[
+            'student_id'=>$faker->randomElement(App\Student::pluck('id')->toArray()),
+            'subject_id'=>$faker->randomElement(App\Subject::pluck('id')->toArray()),
+            'assess'=>$faker->numberBetween($min=2,$max=5),
+    ];
--- database/seeds/GroupTableSeeder.php
+++ database/seeds/GroupTableSeeder.php
@@ -11,6 +11,20 @@ class GroupTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+       
+        
+        // factory(App\Assessment::class,'assessment',4)->create()->each(function($assessment){
+        //         $assessment->subject()->save(factory(App\Subject::class,'assessment')->make());
+        //         // $assessment->student()->save(factory(App\Student::class,'assessment')->make()->each(function($student){
+        //         // $student->group()->save(factory(App\Group::class,'assessment')->make());
+        //         // }));
+        //         $assessment->student()->save(factory(App\Subject::class,'assessment')->make());
+
+        // });
+        factory(App\Assessment::class,'assessment',2)->create()->each(function ($assessment){
+            $assessment->student()->save(factory(App\Student::class,'assessment')->make());
+            $assessment->subject()->save(factory(App\Subject::class,'assessment')->make());
+        });
+
     }
 }
